b5ca9014edd1510e39b6597b9a823c21efef884d
salman3009 <salman444g@gmail.com>
2022-11-04 19:42:33 +0530

    code

--- Question/src/components/App.js
+++ Question/src/components/App.js
@@ -3,22 +3,20 @@ import '../styles/App.css';
 const App = () => {
 
   const handleInput = (event) =>{
-    // console.log(typeof event.target.value);
-   console.log(`Input in #${event.target.id} is ${event.target.value}`);
-
+   // use console.log
   }
 
   // do not change id of input elements
   return (
     <div id="main">
       <label htmlFor='text-input'>Text Input:- </label>
-      <input onChange={handleInput}  id="text-input" type={'text'}  />
+      <input id="text-input" type={'text'} />
 
       <br/>
       <br/>
 
       <label htmlFor='num-input'>Number input</label>
-      <input onChange={handleInput} id="num-input"  type={'number'} />
+      <input id="num-input"  type={'number'} />
       <br/>
     </div>
   )
--- Solution/src/components/App.js
+++ Solution/src/components/App.js
@@ -3,22 +3,18 @@ import '../styles/App.css';
 const App = () => {
 
   const handleInput = (event) =>{
-    // console.log(typeof event.target.value);
-   console.log(`Input in #${event.target.id} is ${event.target.value}`);
-
+    console.log(`Input in #${event.target.id} is ${event.target.value}`)
   }
-
-  // do not change id of input elements
   return (
     <div id="main">
       <label htmlFor='text-input'>Text Input:- </label>
-      <input onChange={handleInput}  id="text-input" type={'text'}  />
+      <input id="text-input" onChange={handleInput} type={'text'} />
 
       <br/>
       <br/>
 
       <label htmlFor='num-input'>Number input</label>
-      <input onChange={handleInput} id="num-input"  type={'number'} />
+      <input id="num-input" onChange={handleInput} type={'number'} />
       <br/>
     </div>
   )
